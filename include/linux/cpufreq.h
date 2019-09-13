265e1078dfce77e50b67c2851308ac0799652846
Santosh Mardi <gsantosh@codeaurora.org>
2019-09-13 17:36:30 +0530

    cpufreq: add CPUFREQ_INCOMPATIBLE event in policy notifiers
    Add support for CPUFREQ_INCOMPATIBLE event in policy notifiers
    this event is added for thermal driver, to make sure thermal
    is the last client in applying the policy limits.
    
    Change-Id: I392e4745957829dd923f1f40201ad5c6b8ccf006
    Signed-off-by: Santosh Mardi <gsantosh@codeaurora.org>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -434,6 +434,7 @@ static inline void cpufreq_resume(void) {}
 /* Policy Notifiers  */
 #define CPUFREQ_ADJUST			(0)
 #define CPUFREQ_NOTIFY			(1)
+#define CPUFREQ_INCOMPATIBLE	(6)
 
 #ifdef CONFIG_ARCH_QCOM
 #define CPUFREQ_THERMAL			(2)
