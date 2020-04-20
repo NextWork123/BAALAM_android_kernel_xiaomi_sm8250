dbf52bd480922b60d5a0e314dc1d3d3f8a36e156
Yaroslav Furman <yaro330@gmail.com>
2020-04-20 10:38:09 +0300

    thermal: cpu_cooling: Simplify cpu_limits_set_level
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    Change-Id: I802cd25298f98e4731b667b2db87895e1091de21

--- drivers/thermal/cpu_cooling.c
+++ drivers/thermal/cpu_cooling.c
@@ -167,18 +167,19 @@ void cpu_limits_set_level(unsigned int cpu, unsigned int max_freq)
 {
 	struct cpufreq_cooling_device *cpufreq_cdev;
 	struct thermal_cooling_device *cdev;
-	unsigned int cdev_cpu;
 	unsigned int level;
 
 	list_for_each_entry(cpufreq_cdev, &cpufreq_cdev_list, node) {
-		sscanf(cpufreq_cdev->cdev->type, "thermal-cpufreq-%d", &cdev_cpu);
-		if (cdev_cpu == cpu) {
+		if (cpufreq_cdev->id == cpu) {
+                  //solve the max CPU limit not set by dongchangsicheng 2022/05/17
 			for (level = 0; level <= cpufreq_cdev->max_level; level++) {
 				int target_freq = cpufreq_cdev->em->table[level].frequency;
 				if (max_freq <= target_freq) {
 					cdev = cpufreq_cdev->cdev;
 					if (cdev)
 						cdev->ops->set_cur_state(cdev, cpufreq_cdev->max_level - level);
+                                        }
+
 					break;
 				}
 			}
